3ffd8906d2a9d182126db4a14028667060098335
angelo-space <theangelospace@gmail.com>
2023-02-26 18:47:08 +0800

    Removed web vitals package, linked Poppins font, added necessary folders, added CSS reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+
 const App = () => {
-  return <div></div>;
+  const findRecipe = async () => {
+    const recipe = await fetch(
+      `https://api.spoonacular.com/recipes/random?number=10&apiKey=${process.env.REACT_APP_RECIPE_KEY}`
+    );
+    const response = await recipe.json();
+    console.log("🚀 ~ file: App.tsx:5 ~ findRecipe ~ response:", response);
+  };
+
+  useEffect(() => {
+    //findRecipe();
+  }, []);
+
+  return <div>Hello Fresh sdwd</div>;
 };
 
 export default App;
